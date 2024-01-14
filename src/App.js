4420ee3bad8822a76f7e908c01b9ec769cd589ed
ksacbibi <34385626+ksacbibi@users.noreply.github.com>
2024-01-14 03:48:54 -0500

    Added currentUser context and notif components

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import {Routes, Route} from "react-router-dom";
 import Pantry from "./componentsnew/Pantry";
 import Notifications from "./componentsnew/Notifications";
 import GroceryList from "./componentsnew/GroceryList";
+import Hero from "./componentsnew/Hero";
 
 
 function App() {
@@ -15,9 +16,9 @@ function App() {
     <Box width={{ base: "100%"}} height="100vh" bgGradient={["linear(to-bl, white, #8CD18F)"]} style={{ margin: 0 }} >
       <VStack spacing={0} p={0} maxW="100%" align="center">
         <Navbar />
-
+        {/* <Kitchen /> */}
         <Routes>
-          <Route path='/' element={<Kitchen/>}/>
+          <Route path='/' element={<Hero/>}/>
           <Route path='/grocerylist' element={<GroceryList/>}/>
           <Route path='/pantry' element={<Pantry/>}/>
           <Route path='/notifications' element={<Notifications/>}/>
