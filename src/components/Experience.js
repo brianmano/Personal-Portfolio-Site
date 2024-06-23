ab8fb60aedc833ad972ae68ef26df36a01bd4fe9
Brian Manomaisupat <brian.manomaisupat@gmail.com>
2024-06-23 17:43:25 -0400

    updated resume:

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -33,8 +33,8 @@ const fontcolour2 = "#eaebea";
 
 const experienceList = [
   {
-    present: false,
-    future: true,
+    present: true,
+    future: false,
     jobTitle: "Telematic Platform Developer",
     companyName: "Scalar Performance Inc.",
     companyDepartment: "Embedded Systems Integration and Configuration",
@@ -124,7 +124,7 @@ export default class Experiences extends Component {
             link with my resume attached:
           </Text>
           <Link
-            href="https://drive.google.com/file/d/1lgre9ghRHktAR7Ym0qdcK-wDvQXLi6eD/view?usp=drive_link"
+            href="https://drive.google.com/file/d/1q6sITnvTjJQpHgGuAHlJx_U3ySFROxPy/view?usp=sharing"
             isExternal
           >
             <Button
--- src/components/IslandNavbar.js
+++ src/components/IslandNavbar.js
@@ -56,7 +56,7 @@ const IslandNavbar = () => {
             ☰
           </MenuButton>
           <MenuList>
-            <Link href="https://drive.google.com/file/d/1lgre9ghRHktAR7Ym0qdcK-wDvQXLi6eD/view?usp=sharing" isExternal>
+            <Link href="https://drive.google.com/file/d/1q6sITnvTjJQpHgGuAHlJx_U3ySFROxPy/view?usp=sharing" isExternal>
               <MenuItem fontFamily="Titillium Web, sans-serif;" variant="link">
                 Resume
               </MenuItem>
